b9766452b360b863576b1e087727238faacf93b0
Brodie Robertson <brodierobertson54321@gmail.com>
2019-07-12 14:24:19 +0930

    Improve slide code

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,33 +1,34 @@
-var slideIndex = 1;
+var slideIndex = 0;
+
+// Cache the slides and dots
+var slides = document.getElementsByClassName("carousel-slide");
+var dots = document.getElementsByClassName("dot");
+
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function cycleSlides(n) {
+    slideIndex += n
+    showSlides(slideIndex);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function switchToSlide(n) {
+    slideIndex = n;
+    showSlides(slideIndex);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("carousel-slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
+    if (n >= slides.length) {
+        slideIndex = 0
     } 
-    if (n < 1) {
-        slideIndex = slides.length
+    if (n < 0) {
+        slideIndex = slides.length - 1
     }
-    for (i = 0; i < slides.length; i++) {
+    for (var i = 0; i < slides.length; ++i) {
         slides[i].style.display = "none"; 
+        dots[i].classList.remove("active")
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block"; 
-
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex].style.display = "block"; 
+    dots[slideIndex].classList.add("active")
 }
